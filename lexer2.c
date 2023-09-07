dfd574233b693f546dbda1ccfb25e1cf6904ef4d
Maria Marques <117844389+macastanm@users.noreply.github.com>
2023-09-07 14:47:01 +0100

    Add files via upload

--- lexer2.c
+++ lexer2.c
@@ -14,22 +14,34 @@
 
 enum e_type	get_type(char *str, int start)
 {
-	if (str[start] == '\"')
+	int	d_q;
+	int s_q;
+
+	d_q = 0;
+	s_q = 0;
+	while (str[start])
+	{
+		if (str[start] == '\"')
+			d_q++;
+		else if (str[start] == '\'')
+			s_q++;
+		else if (str[start] == '|')
+			return (PIPE);
+		else if (str[start] == '<' && str[start + 1] != '<' )
+			return (R_IN);
+		else if (str[start] == '>' && str[start + 1] != '>' )
+			return (R_OUT);
+		else if (str[start] == '<' && str[start + 1] == '<' )
+			return (RR_IN);
+		else if (str[start] == '>' && str[start + 1] == '>' )
+			return (RR_OUT);
+		start++;
+	}
+	if (d_q > 0)
 		return (D_QUOTE);
-	else if (str[start] == '\'')
+	if (s_q > 0)
 		return (S_QUOTE);
-	else if (str[start] == '|')
-		return (PIPE);
-	else if (str[start] == '<' && str[start + 1] != '<' )
-		return (R_IN);
-	else if (str[start] == '>' && str[start + 1] != '>' )
-		return (R_OUT);
-	else if (str[start] == '<' && str[start + 1] == '<' )
-		return (RR_IN);
-	else if (str[start] == '>' && str[start + 1] == '>' )
-		return (RR_OUT);
-	else
-		return (GENERIC);
+	return (GENERIC);
 }
 
 int	pipes_num(t_data *data)
