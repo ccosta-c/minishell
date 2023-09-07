c02d86d6fe4c0054dc066dcd2bf80903eb10b4f3
macastan <mluizacmarques@gmail.com>
2023-09-07 17:32:18 +0100

    cenas redirect and echo

--- execution_echo.c
+++ execution_echo.c
@@ -88,6 +88,8 @@ void	echo_normal(t_data *data)
 			simple_echo(tmp->data);
 		if (tmp->type == D_QUOTE)
 			double_echo(tmp->data);
+		if (tmp->type == SPECIAL_QUOTE)
+			printf("have to do it");
 		else
 		{
 			if (tmp->data[0] == '$' && tmp->data[1] == '?')
