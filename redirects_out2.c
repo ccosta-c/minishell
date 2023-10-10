33182afefa42e603deea43b085b4d39dad435627
macastan <mluizacmarques@gmail.com>
2023-10-10 12:48:24 +0100

    redirects almost almost

--- redirects_out2.c
+++ redirects_out2.c
@@ -96,27 +96,18 @@ int	redi_out_one_node_del(t_data *data, char *str, char *temp)
 
 int	redi_out_one_node(t_data *data, char *str, char *temp)
 {
-	t_tokens	*tmp;
 	int			fd_file;
 
-	tmp = data->top;
-	while (tmp != NULL)
+	(void)str;
+	printf("aquiiii %s\n", temp);
+	fd_file = open(temp, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (fd_file < 0)
 	{
-		if (ft_strcmp(tmp->data, str) == 0)
-		{
-			printf("aquiiii %s\n", temp);
-			fd_file = open(temp, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-			if (fd_file < 0)
-			{
-				printf("Minishell: %s: %s.\n", temp, strerror(errno));
-				return (-1);
-			}
-			dup2(fd_file, STDOUT_FILENO);
-			close(fd_file);
-			data->red_flag++;
-			return (0);
-		}
-		tmp = tmp->next;
+		printf("Minishell: %s: %s.\n", temp, strerror(errno));
+		return (-1);
 	}
+	dup2(fd_file, STDOUT_FILENO);
+	close(fd_file);
+	data->red_flag++;
 	return (0);
 }
