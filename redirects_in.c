06e5874e889afc271edeb49b91f89c6d171feb1c
macastan <mluizacmarques@gmail.com>
2023-10-11 16:32:34 +0100

    redirects in and out done (heredoc and redirect output in append mode) 2

---
+++ redirects_in.c
@@ -0,0 +1,141 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirects_in.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macastan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 11:34:07 by macastan          #+#    #+#             */
+/*   Updated: 2023/10/11 11:34:11 by macastan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes/minishell.h"
+
+int	redi_in(t_data *data, char *str, char *next_s)
+{
+	char	*tmp;
+
+	if (str[0] == '<' && !str[1])
+	{
+		if (redi_out_search(next_s, 0, 0) == 0)
+		{
+			tmp = str_exp_quote(data, next_s, count_str_exp(next_s, 0, 0));
+			if (redi_in_two_nodes(data, str, tmp) == -1)
+				return (free(tmp), -1);
+			return (free(tmp), 0);
+		}
+		else if (redi_out_search(next_s, 0, 0) != 0)
+		{
+			tmp = cut_str(next_s, data, -1, 0);
+			if (redi_in_two_nodes_cut(data, str, tmp) == -1)
+				return (free(tmp), -1);
+			return (free(tmp), 0);
+		}
+	}
+	else if (redi_in2(data, str, next_s) == -1)
+		return (-1);
+	return (0);
+}
+
+int	redi_in4(t_data *data, char *str)
+{
+	char	*tmp;
+
+	if (str[0] == '<' && str[1])
+	{
+		if (redi_out_search(str, 1, 0) == 0)
+		{
+			tmp = change_str(str, data);
+			if (redi_in_one_node_del(data, str, tmp) == -1)
+				return (free(tmp), -1);
+			return (free(tmp), 0);
+		}
+		else if (redi_out_search(str, 1, 0) != 0)
+		{
+			tmp = cut_str_1(str, data, -1, 1);
+			if (redi_in_no_node(data, tmp) == -1)
+				return (free(tmp), -1);
+			return (free(tmp), 0);
+		}
+	}
+	else if (redi_in5(data, str) == -1)
+		return (-1);
+	return (0);
+}
+
+int	redi_in5(t_data *data, char *str)
+{
+	char	*tmp;
+	int		j;
+
+	j = cut_str2(str, 0, 0, 0);
+	if (str[0] != '<'
+		&& (str[ft_strlen(str) - 1] != '>' || str[ft_strlen(str) - 1] != '<'))
+	{
+		if (redi_out_search(str, 0, 0) == 1)
+		{
+			tmp = cut_str_else(str, data, j, 0);
+			if (redi_in_no_node(data, tmp) == -1)
+				return (free(tmp), -1);
+			return (free(tmp), 0);
+		}
+		else if (redi_out_search(str, 0, 0) > 1)
+		{
+			tmp = cut_str_else(str, data, j, 0);
+			if (redi_in_no_node(data, tmp) == -1)
+				return (free(tmp), -1);
+			return (free(tmp), 0);
+		}
+	}
+	return (0);
+}
+
+int	redi_in2(t_data *data, char *str, char *next_s)
+{
+	char	*tmp;
+
+	if (str[0] != '<'
+		&& (str[ft_strlen(str) - 1] == '>' || str[ft_strlen(str) - 1] == '<'))
+	{
+		if (redi_out_search(str, 0, 0) == 1
+			&& redi_out_search(next_s, 0, 0) == 0)
+		{
+			tmp = cut_str_end(str, data, 0, next_s);
+			if (redi_in_one_node_del(data, next_s, tmp) == -1)
+				return (free(tmp), -1);
+			return (free(tmp), 0);
+		}
+		else if (redi_out_search(str, 0, 0) == 1
+			&& redi_out_search(next_s, 0, 0) != 0)
+		{
+			tmp = cut_str_end2(str, data, 0, next_s);
+			if (redi_in_no_node(data, tmp) == -1)
+				return (free(tmp), -1);
+			return (free(tmp), 0);
+		}
+	}
+	else if (redi_in3(data, str) == -1)
+		return (-1);
+	return (0);
+}
+
+int	redi_in3(t_data *data, char *str)
+{
+	char	*tmp;
+
+	if (str[0] != '<'
+		&& (str[ft_strlen(str) - 1] == '>' || str[ft_strlen(str) - 1] == '<'))
+	{
+		if (redi_out_search(str, 0, 0) > 1)
+		{
+			tmp = cut_str_else(str, data, cut_str2(str, 0, 0, 0), 0);
+			if (redi_in_no_node(data, tmp) == -1)
+				return (free(tmp), -1);
+			return (free(tmp), 0);
+		}
+	}
+	else if (redi_in4(data, str) == -1)
+		return (-1);
+	return (0);
+}
---
+++ redirects_in2.c
@@ -0,0 +1,111 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirects_in2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: macastan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 11:34:15 by macastan          #+#    #+#             */
+/*   Updated: 2023/10/11 11:34:20 by macastan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes/minishell.h"
+
+int	redi_in_two_nodes(t_data *data, char *str, char *file)
+{
+	t_tokens	*tmp;
+	int			fd_file;
+
+	tmp = data->top;
+	while (tmp != NULL)
+	{
+		if (ft_strcmp(tmp->data, str) == 0)
+		{
+			fd_file = open(file, O_RDONLY);
+			if (fd_file < 0)
+			{
+				printf("Minishell: %s: %s.\n", file, strerror(errno));
+				return (-1);
+			}
+			dup2(fd_file, STDIN_FILENO);
+			close(fd_file);
+			delete_redirects(&data->top, tmp->next->data);
+			delete_redirects(&data->top, tmp->data);
+			data->red_flag++;
+			return (0);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+int	redi_in_two_nodes_cut(t_data *data, char *str, char *temp)
+{
+	t_tokens	*tmp;
+	int			fd_file;
+
+	tmp = data->top;
+	while (tmp != NULL)
+	{
+		if (ft_strcmp(tmp->data, str) == 0)
+		{
+			fd_file = open(temp, O_RDONLY);
+			if (fd_file < 0)
+			{
+				printf("Minishell: %s: %s.\n", temp, strerror(errno));
+				return (-1);
+			}
+			dup2(fd_file, STDIN_FILENO);
+			close(fd_file);
+			delete_redirects(&data->top, tmp->data);
+			data->red_flag++;
+			return (0);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+int	redi_in_one_node_del(t_data *data, char *str, char *temp)
+{
+	t_tokens	*tmp;
+	int			fd_file;
+
+	tmp = data->top;
+	while (tmp != NULL)
+	{
+		if (ft_strcmp(tmp->data, str) == 0)
+		{
+			fd_file = open(temp, O_RDONLY);
+			if (fd_file < 0)
+			{
+				printf("Minishell: %s: %s.\n", temp, strerror(errno));
+				return (-1);
+			}
+			dup2(fd_file, STDIN_FILENO);
+			close(fd_file);
+			delete_redirects(&data->top, tmp->data);
+			data->red_flag++;
+			return (0);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+int	redi_in_no_node(t_data *data, char *temp)
+{
+	int			fd_file;
+
+	fd_file = open(temp, O_RDONLY);
+	if (fd_file < 0)
+	{
+		printf("Minishell: %s: %s.\n", temp, strerror(errno));
+		return (-1);
+	}
+	dup2(fd_file, STDIN_FILENO);
+	close(fd_file);
+	data->red_flag++;
+	return (0);
+}
